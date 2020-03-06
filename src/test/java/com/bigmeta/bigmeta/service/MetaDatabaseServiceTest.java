1b851dd9e7f9455d1a6f6ca2cbb99f404d85b005
dictator-x <dictator.xwu@gmail.com>
2020-03-05 23:18:20 -0500

    add test

--- src/test/java/com/bigmeta/bigmeta/service/MetaDatabaseServiceTest.java
+++ src/test/java/com/bigmeta/bigmeta/service/MetaDatabaseServiceTest.java
@@ -15,14 +15,23 @@ public class MetaDatabaseServiceTest {
     @Autowired
     private MetaDatabaseService metaDatabaseService;
 
-	@Test
-	public void testSave() {
-		MetaDatabase metaDatabase = new MetaDatabase();
-		metaDatabase.setName("AAAA");
-		metaDatabase.setLocation("hdfs://hadoop000:8020/user/hive/warehouse");
+    @Test
+    public void testSave() {
+        MetaDatabase metaDatabase = new MetaDatabase();
+        metaDatabase.setName("AAAA");
+        metaDatabase.setLocation("hdfs://hadoop000:8020/user/hive/warehouse");
 
-		metaDatabaseService.save(metaDatabase);
-	}
+        metaDatabaseService.save(metaDatabase);
+    }
+
+    @Test
+    public void testQuery() {
+        for ( MetaDatabase metaDatabase : metaDatabaseService.query() ) {
+            System.out.println(metaDatabase.getId());
+            System.out.println(metaDatabase.getName());
+            System.out.println(metaDatabase.getLocation());
+        }
+    }
 
 }
 
